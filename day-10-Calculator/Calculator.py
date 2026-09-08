4f90247df5accae007cffd295372ef2c144a8f8f
Dev Mahatme <mahatme.dev1@gmail.com>
2026-09-08 17:22:17 +0530

    Add files via upload

---
+++ day-10-Calculator/Calculator.py
@@ -0,0 +1,59 @@
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+}
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+# print(operations["*"](4, 8))
+
+def calculator():
+    print(logo)
+    should_accumulate = True
+    f_number = float(input("What's the first number?: "))
+
+    while should_accumulate:
+
+        for k in operations:
+            print(k)
+        op = input("Pick an operation: ")
+        s_number = float(input("What's the next number?: "))
+
+        answer = operations[op](f_number, s_number)
+        print(f"{f_number} {op} {s_number} = {answer}")
+
+        again = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ").lower()
+        if  again == "y":
+            f_number = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
